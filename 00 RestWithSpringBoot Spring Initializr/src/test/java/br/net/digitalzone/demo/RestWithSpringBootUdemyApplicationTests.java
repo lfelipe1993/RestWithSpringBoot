dfcf38266fc0f9605784b13d11fe065657a2187a
Luiz Felipe <lfelipe1993@live.com>
2020-04-14 00:46:05 -0300

    Hello SpringBoot and REST

---
+++ 00 RestWithSpringBoot Spring Initializr/src/test/java/br/net/digitalzone/demo/RestWithSpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.digitalzone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
