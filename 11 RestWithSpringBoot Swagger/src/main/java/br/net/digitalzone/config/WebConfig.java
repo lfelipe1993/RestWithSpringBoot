8e4318be66285b8e2cd2798197b38f9d4bd9bfd6
Luiz Felipe <lfelipe1993@live.com>
2020-04-15 11:22:19 -0300

    Swagger Support

---
+++ 11 RestWithSpringBoot Swagger/src/main/java/br/net/digitalzone/config/WebConfig.java
@@ -0,0 +1,54 @@
+package br.net.digitalzone.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import br.net.digitalzone.serialization.converter.YamlJackson2HttpMessageConverter;
+
+@Configuration
+//@EnableWebMvc // suporta spring mvc na sua app e tambem vao subir instancia do sprinboot e tem acesso ao jackson
+public class WebConfig implements WebMvcConfigurer {
+	
+	private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
+	
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters ){
+		converters.add(new YamlJackson2HttpMessageConverter());
+		
+	}
+	
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		
+		/*VIA EXTENSAO configurer.favorParameter(false)
+		.ignoreAcceptHeader(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML);*/
+		
+		/*
+		 * VIA QUERY PARAM configurer.favorPathExtension(false) .favorParameter(true)
+		 * .parameterName("MediaType") .ignoreAcceptHeader(true)
+		 * .useRegisteredExtensionsOnly(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+		 */
+		
+		
+		//header
+		configurer.favorPathExtension(false)
+		.favorParameter(false)
+		.ignoreAcceptHeader(false)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML)
+		.mediaType("xml", MEDIA_TYPE_YML);
+		
+	}
+
+}
