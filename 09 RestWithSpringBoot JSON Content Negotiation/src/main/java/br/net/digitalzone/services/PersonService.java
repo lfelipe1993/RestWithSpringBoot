a378ea5b7d29caebbb0b8ec1f1179da9dbec0148
Luiz Felipe <lfelipe1993@live.com>
2020-04-15 00:34:30 -0300

    Content Negotiation (YAML)

---
+++ 09 RestWithSpringBoot JSON Content Negotiation/src/main/java/br/net/digitalzone/services/PersonService.java
@@ -0,0 +1,58 @@
+package br.net.digitalzone.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.net.digitalzone.converter.DozerConverter;
+import br.net.digitalzone.data.model.Person;
+import br.net.digitalzone.data.vo.v1.PersonVO;
+import br.net.digitalzone.exception.ResourceNotFoundException;
+import br.net.digitalzone.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public PersonVO create(PersonVO person) {
+		
+		var entity = DozerConverter.parseObject(person, Person.class);
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
+		
+		var entity = repository.findById(person.getId()).orElseThrow(() -> 
+		new ResourceNotFoundException("No records found for this ID"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+	}
+	
+	public void delete(Long id) {
+		Person entity = repository.findById(id).orElseThrow(() -> 
+		new ResourceNotFoundException("No records found for this ID"));
+		
+		repository.delete(entity);
+	}
+	
+	public PersonVO findById(Long id) {
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
+	
+	public List<PersonVO> findAll() {
+		return DozerConverter.parseListObjects(repository.findAll(),PersonVO.class);
+	}
+
+}
